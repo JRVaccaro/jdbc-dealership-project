a89705da21c6feb365465fdf85a4848b19ee3d4c
JRVaccaro <ooliebud@gmail.com>
2025-06-13 23:03:45 -0400

    Added addLeaseContract method to LeaseDao with basic implementation. Placeholder for lease start and end dates, will fix conversion

--- src/main/java/com/pluralsight/dealership/DataBase/LeaseDao.java
+++ src/main/java/com/pluralsight/dealership/DataBase/LeaseDao.java
@@ -16,5 +16,21 @@ public class LeaseDao {
 
     public void addLeaseContract(LeaseContract leaseContract) {
         // TODO: Implement the logic to add a lease contract
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(
+                     "INSERT INTO lease_contract (contract_id, vin, lease_start, lease_end, monthly_pay) VALUES (?, ?, ?, ? ,?)")){
+
+            statement.setInt(1, leaseContract.getContractId());
+            statement.setString(2, leaseContract.getVin());
+            statement.setDate(3, leaseContract.getLeaseStart(); //I'm unsure, will come back to
+            statement.setDate (4, leaseContract.getLeaseEnd(); //I'm unsure, will come back to
+            statement.setDouble(5, leaseContract.getMonthlyPayment());
+
+            statement.executeUpdate();
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
