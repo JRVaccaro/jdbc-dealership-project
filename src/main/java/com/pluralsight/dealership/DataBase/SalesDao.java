1bdb92323bf761bdc4e5fae79fa4e15646a818b0
JRVaccaro <ooliebud@gmail.com>
2025-06-16 23:53:30 -0400

    Fixed addSalesContract in SalesDao.

--- src/main/java/com/pluralsight/dealership/DataBase/SalesDao.java
+++ src/main/java/com/pluralsight/dealership/DataBase/SalesDao.java
@@ -5,6 +5,7 @@ import com.pluralsight.dealership.models.SalesContract;
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class SalesDao {
@@ -16,16 +17,33 @@ public class SalesDao {
     }
 
     public void addSalesContract(SalesContract salesContract) {
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement statement = connection.prepareStatement(
-                     "INSERT INTO sales_contracts (contract_id, vin,sale_date, price) VALUES (?,?,?,?)")){
+        try (Connection connection = dataSource.getConnection()) {
 
-            statement.setInt(1, salesContract.getContractId());
-            statement.setString(2, salesContract.getVin());
-            statement.setDate(3,java.sql.Date.valueOf(salesContract.getSaleDate())); //Makes the sale date into a SQL format so it can be saved in the database
-            statement.setDouble(4, salesContract.getPrice());
-        } catch (SQLException e) {
-            e.printStackTrace();
+            try (PreparedStatement statement = connection.prepareStatement(
+                    "SELECT VIN FROM Vehicles WHERE VIN = ?")) {
+                statement.setString(1, salesContract.getVin());
+                ResultSet resultSet = statement.executeQuery();
+
+                if (!resultSet.next()) {
+                    System.out.println("Error! VIN does not exist.");
+                    return;
+                }
+
+            }
+
+
+            try (PreparedStatement statement = connection.prepareStatement(
+                    "INSERT INTO sales_contracts (contract_id, vin,sale_date, price) VALUES (?,?,?,?)")) {
+
+                statement.setInt(1, salesContract.getContractId());
+                statement.setString(2, salesContract.getVin());
+                statement.setDate(3, java.sql.Date.valueOf(salesContract.getSaleDate())); //Makes the sale date into a SQL format so it can be saved in the database
+                statement.setDouble(4, salesContract.getPrice());
+
+                statement.executeUpdate();
+            }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
     }
-}
